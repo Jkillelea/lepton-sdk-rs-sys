a6f20c1e33afdf1819d86873d935311e6111e7eb
Jacob Killelea <Jkillelea@protonmail.ch>
2019-03-04 09:54:26 -0700

    renames

--- src/error_codes.rs
+++ src/error_codes.rs
@@ -1,5 +1,4 @@
 /// Mod Error Codes
-
 use super::bindings;
 use super::bindings::*;
 
@@ -8,91 +7,90 @@ use super::bindings::*;
 /// two enum variants be represented by the same integer.
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
 #[repr(i32)]
-pub enum LeptonError {
-    Ok = Result_LEP_OK,
-    Error = Result_LEP_ERROR,
-    NotReady = Result_LEP_NOT_READY,
-    RangeError = Result_LEP_RANGE_ERROR,
-    ChecksumError = Result_LEP_CHECKSUM_ERROR,
-    BadArgPointerError = Result_LEP_BAD_ARG_POINTER_ERROR,
-    DataSizeError = Result_LEP_DATA_SIZE_ERROR,
-    UndefinedFunctionError =Result_LEP_UNDEFINED_FUNCTION_ERROR,
-    FunctionNotSupported = Result_LEP_FUNCTION_NOT_SUPPORTED,
-    OTPWriteError = Result_LEP_OTP_WRITE_ERROR,
-    OTPReadError = Result_LEP_OTP_READ_ERROR,
-    OTPNotProgrammedError = Result_LEP_OTP_NOT_PROGRAMMED_ERROR,
-    I2CBusNotReady = Result_LEP_ERROR_I2C_BUS_NOT_READY,
-    I2CBufferOverflow = Result_LEP_ERROR_I2C_BUFFER_OVERFLOW,
-    I2CArbitrationLost = Result_LEP_ERROR_I2C_ARBITRATION_LOST,
-    I2CBusError = Result_LEP_ERROR_I2C_BUS_ERROR,
-    I2CNackRecieved = Result_LEP_ERROR_I2C_NACK_RECEIVED,
-    I2CFail = Result_LEP_ERROR_I2C_FAIL,
-    DivZeroError = Result_LEP_DIV_ZERO_ERROR,
-    CommPortNotOpen = Result_LEP_COMM_PORT_NOT_OPEN,
-    CommInvalidPortError = Result_LEP_COMM_INVALID_PORT_ERROR,
-    CommRangeError = Result_LEP_COMM_RANGE_ERROR,
-    ErrorCreatingComm = Result_LEP_ERROR_CREATING_COMM,
-    ErrorStartingComm = Result_LEP_ERROR_STARTING_COMM,
-    ErrorClosingComm = Result_LEP_ERROR_CLOSING_COMM,
-    CommChecksumError = Result_LEP_COMM_CHECKSUM_ERROR,
-    CommNoDevice = Result_LEP_COMM_NO_DEV,
-    TimeoutError = Result_LEP_TIMEOUT_ERROR,
-    ErrorWritingComm = Result_LEP_COMM_ERROR_WRITING_COMM,
-    ErrorReadingComm = Result_LEP_COMM_ERROR_READING_COMM,
-    CommCountError = Result_LEP_COMM_COUNT_ERROR,
-    OperationCanceled = Result_LEP_OPERATION_CANCELED,
-    UndefinedError = Result_LEP_UNDEFINED_ERROR_CODE,
+pub enum LeptonResult {
+    Ok                     = Result_LEP_OK,
+    Error                  = Result_LEP_ERROR,
+    NotReady               = Result_LEP_NOT_READY,
+    RangeError             = Result_LEP_RANGE_ERROR,
+    ChecksumError          = Result_LEP_CHECKSUM_ERROR,
+    BadArgPointerError     = Result_LEP_BAD_ARG_POINTER_ERROR,
+    DataSizeError          = Result_LEP_DATA_SIZE_ERROR,
+    UndefinedFunctionError = Result_LEP_UNDEFINED_FUNCTION_ERROR,
+    FunctionNotSupported   = Result_LEP_FUNCTION_NOT_SUPPORTED,
+    OTPWriteError          = Result_LEP_OTP_WRITE_ERROR,
+    OTPReadError           = Result_LEP_OTP_READ_ERROR,
+    OTPNotProgrammedError  = Result_LEP_OTP_NOT_PROGRAMMED_ERROR,
+    I2CBusNotReady         = Result_LEP_ERROR_I2C_BUS_NOT_READY,
+    I2CBufferOverflow      = Result_LEP_ERROR_I2C_BUFFER_OVERFLOW,
+    I2CArbitrationLost     = Result_LEP_ERROR_I2C_ARBITRATION_LOST,
+    I2CBusError            = Result_LEP_ERROR_I2C_BUS_ERROR,
+    I2CNackRecieved        = Result_LEP_ERROR_I2C_NACK_RECEIVED,
+    I2CFail                = Result_LEP_ERROR_I2C_FAIL,
+    DivZeroError           = Result_LEP_DIV_ZERO_ERROR,
+    CommPortNotOpen        = Result_LEP_COMM_PORT_NOT_OPEN,
+    CommInvalidPortError   = Result_LEP_COMM_INVALID_PORT_ERROR,
+    CommRangeError         = Result_LEP_COMM_RANGE_ERROR,
+    ErrorCreatingComm      = Result_LEP_ERROR_CREATING_COMM,
+    ErrorStartingComm      = Result_LEP_ERROR_STARTING_COMM,
+    ErrorClosingComm       = Result_LEP_ERROR_CLOSING_COMM,
+    CommChecksumError      = Result_LEP_COMM_CHECKSUM_ERROR,
+    CommNoDevice           = Result_LEP_COMM_NO_DEV,
+    TimeoutError           = Result_LEP_TIMEOUT_ERROR,
+    ErrorWritingComm       = Result_LEP_COMM_ERROR_WRITING_COMM,
+    ErrorReadingComm       = Result_LEP_COMM_ERROR_READING_COMM,
+    CommCountError         = Result_LEP_COMM_COUNT_ERROR,
+    OperationCanceled      = Result_LEP_OPERATION_CANCELED,
+    UndefinedError         = Result_LEP_UNDEFINED_ERROR_CODE,
 }
 
 #[allow(dead_code)]
-impl LeptonError {
+impl LeptonResult {
     fn is_ok(self) -> bool {
-        self == LeptonError::Ok
+        self == LeptonResult::Ok
     }
 }
 
-impl std::convert::From<bindings::Result> for LeptonError {
+impl std::convert::From<bindings::Result> for LeptonResult {
     // Looks like the best way to convert from a C enum (integer) to a Rust enum
     // is still a lookup table
     #[allow(non_upper_case_globals)]
     fn from(result: bindings::Result) -> Self {
         match result {
-            Result_LEP_OK => LeptonError::Ok,
-            // Result_LEP_COMM_OK => LeptonError::Ok,
-            Result_LEP_ERROR => LeptonError::Error,
-            Result_LEP_NOT_READY => LeptonError::NotReady,
-            Result_LEP_RANGE_ERROR => LeptonError::RangeError ,
-            Result_LEP_CHECKSUM_ERROR => LeptonError::ChecksumError ,
-            Result_LEP_BAD_ARG_POINTER_ERROR => LeptonError::BadArgPointerError ,
-            Result_LEP_DATA_SIZE_ERROR => LeptonError::DataSizeError ,
-            Result_LEP_UNDEFINED_FUNCTION_ERROR => LeptonError::UndefinedFunctionError ,
-            Result_LEP_FUNCTION_NOT_SUPPORTED => LeptonError::FunctionNotSupported ,
-            Result_LEP_OTP_WRITE_ERROR => LeptonError::OTPWriteError ,
-            Result_LEP_OTP_READ_ERROR => LeptonError::OTPReadError ,
-            Result_LEP_OTP_NOT_PROGRAMMED_ERROR => LeptonError::OTPNotProgrammedError ,
-            Result_LEP_ERROR_I2C_BUS_NOT_READY => LeptonError::I2CBusNotReady ,
-            Result_LEP_ERROR_I2C_BUFFER_OVERFLOW => LeptonError::I2CBufferOverflow ,
-            Result_LEP_ERROR_I2C_ARBITRATION_LOST => LeptonError::I2CArbitrationLost ,
-            Result_LEP_ERROR_I2C_BUS_ERROR => LeptonError::I2CBusError ,
-            Result_LEP_ERROR_I2C_NACK_RECEIVED => LeptonError::I2CNackRecieved ,
-            Result_LEP_ERROR_I2C_FAIL => LeptonError::I2CFail ,
-            Result_LEP_DIV_ZERO_ERROR => LeptonError::DivZeroError ,
-            Result_LEP_COMM_PORT_NOT_OPEN => LeptonError::CommPortNotOpen ,
-            Result_LEP_COMM_INVALID_PORT_ERROR => LeptonError::CommInvalidPortError ,
-            Result_LEP_COMM_RANGE_ERROR => LeptonError::CommRangeError ,
-            Result_LEP_ERROR_CREATING_COMM => LeptonError::ErrorCreatingComm ,
-            Result_LEP_ERROR_STARTING_COMM => LeptonError::ErrorStartingComm ,
-            Result_LEP_ERROR_CLOSING_COMM => LeptonError::ErrorClosingComm ,
-            Result_LEP_COMM_CHECKSUM_ERROR => LeptonError::CommChecksumError ,
-            Result_LEP_COMM_NO_DEV => LeptonError::CommNoDevice ,
-            Result_LEP_TIMEOUT_ERROR => LeptonError::TimeoutError ,
-            Result_LEP_COMM_ERROR_WRITING_COMM => LeptonError::ErrorWritingComm ,
-            Result_LEP_COMM_ERROR_READING_COMM => LeptonError::ErrorReadingComm ,
-            Result_LEP_COMM_COUNT_ERROR => LeptonError::CommCountError ,
-            Result_LEP_OPERATION_CANCELED => LeptonError::OperationCanceled,
-            Result_LEP_UNDEFINED_ERROR_CODE => LeptonError::UndefinedError,
+            Result_LEP_OK                         => LeptonResult::Ok,
+            // Result_LEP_COMM_OK                 => LeptonError::Ok,
+            Result_LEP_ERROR                      => LeptonResult::Error,
+            Result_LEP_NOT_READY                  => LeptonResult::NotReady,
+            Result_LEP_RANGE_ERROR                => LeptonResult::RangeError,
+            Result_LEP_CHECKSUM_ERROR             => LeptonResult::ChecksumError,
+            Result_LEP_BAD_ARG_POINTER_ERROR      => LeptonResult::BadArgPointerError,
+            Result_LEP_DATA_SIZE_ERROR            => LeptonResult::DataSizeError,
+            Result_LEP_UNDEFINED_FUNCTION_ERROR   => LeptonResult::UndefinedFunctionError,
+            Result_LEP_FUNCTION_NOT_SUPPORTED     => LeptonResult::FunctionNotSupported,
+            Result_LEP_OTP_WRITE_ERROR            => LeptonResult::OTPWriteError,
+            Result_LEP_OTP_READ_ERROR             => LeptonResult::OTPReadError,
+            Result_LEP_OTP_NOT_PROGRAMMED_ERROR   => LeptonResult::OTPNotProgrammedError,
+            Result_LEP_ERROR_I2C_BUS_NOT_READY    => LeptonResult::I2CBusNotReady,
+            Result_LEP_ERROR_I2C_BUFFER_OVERFLOW  => LeptonResult::I2CBufferOverflow,
+            Result_LEP_ERROR_I2C_ARBITRATION_LOST => LeptonResult::I2CArbitrationLost,
+            Result_LEP_ERROR_I2C_BUS_ERROR        => LeptonResult::I2CBusError,
+            Result_LEP_ERROR_I2C_NACK_RECEIVED    => LeptonResult::I2CNackRecieved,
+            Result_LEP_ERROR_I2C_FAIL             => LeptonResult::I2CFail,
+            Result_LEP_DIV_ZERO_ERROR             => LeptonResult::DivZeroError,
+            Result_LEP_COMM_PORT_NOT_OPEN         => LeptonResult::CommPortNotOpen,
+            Result_LEP_COMM_INVALID_PORT_ERROR    => LeptonResult::CommInvalidPortError,
+            Result_LEP_COMM_RANGE_ERROR           => LeptonResult::CommRangeError,
+            Result_LEP_ERROR_CREATING_COMM        => LeptonResult::ErrorCreatingComm,
+            Result_LEP_ERROR_STARTING_COMM        => LeptonResult::ErrorStartingComm,
+            Result_LEP_ERROR_CLOSING_COMM         => LeptonResult::ErrorClosingComm,
+            Result_LEP_COMM_CHECKSUM_ERROR        => LeptonResult::CommChecksumError,
+            Result_LEP_COMM_NO_DEV                => LeptonResult::CommNoDevice,
+            Result_LEP_TIMEOUT_ERROR              => LeptonResult::TimeoutError,
+            Result_LEP_COMM_ERROR_WRITING_COMM    => LeptonResult::ErrorWritingComm,
+            Result_LEP_COMM_ERROR_READING_COMM    => LeptonResult::ErrorReadingComm,
+            Result_LEP_COMM_COUNT_ERROR           => LeptonResult::CommCountError,
+            Result_LEP_OPERATION_CANCELED         => LeptonResult::OperationCanceled,
+            Result_LEP_UNDEFINED_ERROR_CODE       => LeptonResult::UndefinedError,
             _ => panic!("Unexpected Lepton Result!")
-
         }
     }
 }
