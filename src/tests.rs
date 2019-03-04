d3d9bdcf3ffa298e6dc6c3f7e8c59558eb175e56
Jacob Killelea <Jkillelea@protonmail.ch>
2019-03-04 09:57:45 -0700

    tests file

---
+++ src/tests.rs
@@ -0,0 +1,22 @@
+// mod tests
+
+use super::bindings::*;
+use super::error_codes::LeptonResult;
+use super::PortTag;
+
+#[test]
+fn test_open_port() {
+    let port_id   = 1; // 1 for /dev/i2c-1, 0 for /dev/i2c-0
+    let port_type = PortTag::CciTwi;
+    let baud_rate = 400; // kHz
+    let mut port: LEP_CAMERA_PORT_DESC_T_TAG = unsafe { std::mem::zeroed() };
+    let result: LeptonResult;
+
+    unsafe {
+        result = LEP_OpenPort(port_id, port_type as u32, baud_rate, &mut port).into();
+    }
+
+    println!("{:#?}", port);
+
+    assert_eq!(result, LeptonResult::Ok);
+}
