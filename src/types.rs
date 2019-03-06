5f505b30c6623fce67ca37b7170eb1618b8595aa
Jacob Killelea <Jkillelea@protonmail.ch>
2019-03-06 11:38:01 -0700

    misc documentation

--- src/types.rs
+++ src/types.rs
@@ -2,12 +2,14 @@
 
 use super::bindings::*;
 
+#[derive(Debug)]
 #[repr(u32)]
 pub enum ByteOrder {
     LSBFirst = LEP_BYTE_ORDER_T_LEP_LSB_FIRST,
     MSBFirst = LEP_BYTE_ORDER_T_LEP_MSB_FIRST,
 }
 
+#[derive(Debug)]
 #[repr(u32)]
 pub enum OperationState {
     Ready = LEP_OPERATION_STATE_LEP_READY,
@@ -15,24 +17,34 @@ pub enum OperationState {
     Waiting = LEP_OPERATION_STATE_LEP_WAITING,
 }
 
+/// Radiometry enable/disable
+#[derive(Debug)]
 #[repr(u32)]
 pub enum EnableState {
     Disabled = LEP_ENABLE_STATE_LEP_DISABLED,
     Enabled = LEP_ENABLE_STATE_LEP_ENABLED,
 }
 
+#[derive(Debug)]
 #[repr(u32)]
 pub enum OnState {
     Off = LEP_ON_STATE_LEP_OFF,
     On = LEP_ON_STATE_LEP_ON,
 }
 
+/// Only commands over TWI (I2C) are implemented.
+///
+/// TWI is identically equal to the I2C protocol, but the name isn't
+/// copyrighted. Atmel also calls their implementation the same thing.
+#[derive(Debug)]
 #[repr(u32)]
 pub enum PortType {
     CciTwi = LEP_CAMERA_PORT_E_TAG_LEP_CCI_TWI,
     CciSpi = LEP_CAMERA_PORT_E_TAG_LEP_CCI_SPI, // unimplemented I think
 }
 
+/// Default appears to be 400kHz
+#[derive(Debug)]
 #[repr(u32)]
 pub enum TwiClockRate {
     Khz100 = LEP_TWI_CLOCK_RATE_T_TAG_LEP_TWI_CLOCK_100KHZ,
@@ -40,6 +52,7 @@ pub enum TwiClockRate {
     Mhz1   = LEP_TWI_CLOCK_RATE_T_TAG_LEP_TWI_CLOCK_1MHZ,
 }
 
+#[derive(Debug)]
 #[repr(u32)]
 pub enum SpiClockRate {
     Mhz2  = LEP_SPI_CLOCK_RATE_T_TAG_LEP_SPI_CLOCK_2MHZ,
@@ -47,15 +60,7 @@ pub enum SpiClockRate {
     Mhz20 = LEP_SPI_CLOCK_RATE_T_TAG_LEP_SPI_CLOCK_20MHZ,
 }
 
-// #[repr(C)]
-// pub struct CameraPort {
-//     port_id:   u16,
-//     port_type: PortType,
-//     baud_rate: u16,
-//     dev_addr:  u8,
-// }
-
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug)]
 #[repr(u32)]
 pub enum PortTag {
     CciTwi = LEP_CAMERA_PORT_E_TAG_LEP_CCI_TWI, 
