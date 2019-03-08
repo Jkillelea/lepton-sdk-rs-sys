6af0b8e1ba94d1e1272b1c3346df6c4dac103f64
Jacob Killelea <Jkillelea@protonmail.ch>
2019-03-07 20:12:52 -0700

    layout

--- src/tests.rs
+++ src/tests.rs
@@ -5,11 +5,17 @@ use super::*;
 
 #[test]
 fn test_open_port_manually() {
+    let result: LeptonResult;
     let port_id   = 1; // 1 for /dev/i2c-1, 0 for /dev/i2c-0
     let port_type = PortTag::CciTwi;
     let baud_rate = 400; // kHz
-    let mut port: LEP_CAMERA_PORT_DESC_T_TAG = unsafe { std::mem::zeroed() };
-    let result: LeptonResult;
+
+    let mut port = LEP_CAMERA_PORT_DESC_T_TAG {
+        portID: 1,                        // /dev/i2c-1
+        portType: PortTag::CciTwi as u32, // TWI
+        portBaudRate: 400,                // kHz
+        deviceAddress: 0x2A,
+    };
 
     unsafe {
         result = LEP_OpenPort(port_id, 
